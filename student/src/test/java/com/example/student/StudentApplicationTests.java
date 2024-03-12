21e72530de1057b45ecb3db7ddcda8e2e3ec5985
ShyamSundarG <shyamsundarg@gmail.com>
2024-03-12 13:42:38 +0530

    sonar fix

--- student/src/test/java/com/example/student/StudentApplicationTests.java
+++ student/src/test/java/com/example/student/StudentApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.student;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("sonar-exclude")
 class StudentApplicationTests {
 
 	@Test
